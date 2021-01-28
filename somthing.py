cfb70b9b65c957ff88f913cef0a59650956c20ad
787ui <77878795+787ui@users.noreply.github.com>
2021-01-28 06:27:05 -0300

    Create somthing.py

---
+++ somthing.py
@@ -0,0 +1,13 @@
+in_range = true
+if in_range = true:
+ import pandas as pd
+ import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data)
+
+print(df) 
